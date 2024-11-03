3800a64651a1f4b8197286423098e68c6e5f812d
Gia Phuc <giaphuc29082004@gmail.com>
2024-11-03 13:28:31 +0700

    update

--- Student.java
+++ Student.java
@@ -1,3 +1,16 @@
 public class Student{
-	
+    String name;
+    String RollNo;
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getRollNo(){
+        return RollNo;
+    }
+    public void setRollNo(String RollNo){
+        this.RollNo = RollNo;
+    }
 }
--- codesmell.java
+++ codesmell.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class ShoppingCart {
     private List<String> items = new ArrayList<>();
     
